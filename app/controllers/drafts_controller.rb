7c4fa53930a206a587052a3d241b445836db5337
Marcelo F Andrade <mfandrade@gmail.com>
2014-08-07 18:20:59 -0300

    Remove unneded json support

--- app/controllers/drafts_controller.rb
+++ app/controllers/drafts_controller.rb
@@ -22,43 +22,28 @@ class DraftsController < ApplicationController
   end
 
   # POST /drafts
-  # POST /drafts.json
   def create
     @draft = Draft.new(draft_params)
-
-    respond_to do |format|
-      if @draft.save
-        format.html { redirect_to @draft, notice: 'Draft was successfully created.' }
-        format.json { render :show, status: :created, location: @draft }
-      else
-        format.html { render :new }
-        format.json { render json: @draft.errors, status: :unprocessable_entity }
-      end
+    if @draft.save
+      redirect_to @draft, notice: 'Draft was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /drafts/1
-  # PATCH/PUT /drafts/1.json
   def update
-    respond_to do |format|
-      if @draft.update(draft_params)
-        format.html { redirect_to @draft, notice: 'Draft was successfully updated.' }
-        format.json { render :show, status: :ok, location: @draft }
-      else
-        format.html { render :edit }
-        format.json { render json: @draft.errors, status: :unprocessable_entity }
-      end
+    if @draft.update(draft_params)
+      redirect_to @draft, notice: 'Draft was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /drafts/1
-  # DELETE /drafts/1.json
   def destroy
     @draft.destroy
-    respond_to do |format|
-      format.html { redirect_to drafts_url, notice: 'Draft was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to drafts_url, notice: 'Draft was successfully destroyed.'
   end
 
   private
